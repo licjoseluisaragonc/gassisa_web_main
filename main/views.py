354ccaf0f2bb20c0c620648c95a568e5381b3355
Jose Luis Aragon C. <gassisamexico@gmail.com>
2025-06-13 15:39:07 -0600

    update
    ok

--- main/views.py
+++ main/views.py
@@ -1,6 +1,18 @@
+import os
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 
 # Vistas de aplicación main index
 def IndexView(request):
     return render(request, "index.html")
+
+def list_staticfiles(request):
+    static_dir = settings.STATIC_ROOT
+    files = []
+    for root, dirs, filenames in os.walk(static_dir):
+        for filename in filenames:
+            filepath = os.path.relpath(os.path.join(root, filename), static_dir)
+            files.append(filepath)
+    return JsonResponse({'static_files': files})
